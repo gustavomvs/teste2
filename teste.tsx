8c09cc6fc1061d236f80acf0fef5b83ec7a95e8a
Gustavo Moura <91807686+gustavomvs@users.noreply.github.com>
2024-05-24 12:32:51 -0300

    Update teste.tsx

--- teste.tsx
+++ teste.tsx
@@ -2,6 +2,6 @@ import * as React from "react";
 
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 const teste = () => {
-  return <div>Hello World 3</div>;
+  return <div>Hello World 4</div>;
 };
 export default teste;
